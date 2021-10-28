c9e1ad60406d9d6817c616c3975b79df1bfc5f9c
bazman11 <bazman1@etf.unsa.ba>
2021-10-28 13:50:12 +0200

    Dodana metoda dodajArtikl

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String imeArtikla;
+    private int cijena;
+    private String kod;
+
+    public Artikl(String njegovoIme, int c, String k){
+        imeArtikla=njegovoIme;
+        cijena=c;
+        kod=k;
+    }
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl[] artikli=new Artikl[50];
+    static int i=0;
+    public boolean dodajArtikl(Artikl k){
+        if (i>50) return false;
+        else {
+            artikli[i]=k;
+            i++;
+            return true;
+        }
+    }
 }
