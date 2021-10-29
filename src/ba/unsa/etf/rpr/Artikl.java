41893cdc2d2977b6cf4888f9f13372f67a6607ec
bazman11 <bazman1@etf.unsa.ba>
2021-10-29 10:17:32 +0200

    Dovrsen zadatak

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,13 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
-    private String imeArtikla;
-    private int cijena;
+    private String naziv;
     private String kod;
+    private int cijena;
 
-    public Artikl(String njegovoIme, int c, String k){
-        imeArtikla=njegovoIme;
-        cijena=c;
-        kod=k;
+    public Artikl(String naziv, int cijena, String kod) {
+        this.naziv = naziv;
+        this.cijena = cijena;
+        this.kod = kod;
+    }
+
+    public Artikl() {
+        this.naziv="";
+        this.cijena = 0;
+        this.kod="";
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+    public int getCijena() {
+        return cijena;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
+
+    public void setCijena(int cijena) {
+        this.cijena = cijena;
     }
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,37 +1,42 @@
 package ba.unsa.etf.rpr;
-import java.lang.String;
+
 
 public class Korpa {
-    Artikl[] artikli=new Artikl[50];
-    static int i=0;
-    public boolean dodajArtikl(Artikl k){
-        if(i>50) return false;
-        else{
-            artikli[i]=k;
-            i++;
-            return  true;
-        }
+    private Artikl[] artikli;
+    private int kapacitet, brojArtikala;
+
+    public Korpa() {
+        this.artikli=new Artikl[50];
+        this.kapacitet=50;
+        this.brojArtikala=0;
     }
-    public Artikl izbaciArtiklSaKodom(String k){
-        for(int j=0; j<i; j++){
-            if(artikli[j].kod.equals(k)){
-                Artikl t=artikli[j];
-                artikli[j]=artikli[i-1];
-                artikli[i-1]=null;
-                i--;
-                return t;
-            }
-        }
-        return null;
-    }
-    public Artikl[] getArtikli(){
+
+    public Artikl[] getArtikli() {
         return artikli;
     }
-    public int dajUkupnuCijenuArtikala(){
-        int ukupna=0;
-        for(int j=0; j<i; j++){
-            ukupna=ukupna+artikli[j].cijena;
+    public boolean dodajArtikl(Artikl a){
+        if(brojArtikala == kapacitet) return false;
+        artikli[brojArtikala] = a;
+        brojArtikala++;
+        return true;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod){
+        Artikl a = new Artikl();
+        for(int i = 0; i<brojArtikala; i++){
+            if(artikli[i].getKod().equalsIgnoreCase(kod)){
+                a=artikli[i];
+                for(int j=i;j<brojArtikala;j++)
+                    artikli[j] = artikli[j+1];
+            }
+            brojArtikala--;
         }
-        return ukupna;
+        return a;
+    }
+
+    public int dajUkupnuCijenuArtikala(){
+        int suma=0;
+        for (int i = 0; i < brojArtikala; i++)
+            suma+=artikli[i].getCijena();
+        return suma;
     }
 }
